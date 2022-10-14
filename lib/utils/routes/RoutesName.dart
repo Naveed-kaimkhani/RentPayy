a19181f972f0afeac497b26bfcd20f6b70c6053b
Osama Malik <93467529+osamamalik234@users.noreply.github.com>
2022-10-14 11:33:01 +0500

    RoutesName updated

--- lib/utils/routes/RoutesName.dart
+++ lib/utils/routes/RoutesName.dart
@@ -1,7 +1,6 @@
 class RoutesName{
  static const String login = 'login_screen';
  static const String homeScreen = 'home_screen';
- static const String SellerCatagory = 'SellerCatagory';
  static const String userSellerScreen = 'UserSellerScreen';
 
 }
