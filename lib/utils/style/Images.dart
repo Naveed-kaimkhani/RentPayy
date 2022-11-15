931473a22b37e723af1c062b30a77cc695b379c5
Osama Malik <93467529+osamamalik234@users.noreply.github.com>
2022-11-14 23:38:34 -0800

    images updated

--- lib/utils/style/Images.dart
+++ lib/utils/style/Images.dart
@@ -1,4 +1,6 @@
 
+import 'dart:core';
+
 class Images {
   Images._();
   // vector icons
@@ -8,12 +10,26 @@ class Images {
   static const String or= 'asset/or.png';
    static const String google= 'asset/Google.png';
      static const String facebook= 'asset/facebook.png';
-     static const String favourite = 'asset/favourite.png';
-     static const String share = 'asset/share.png';
-     static const String location = 'asset/location.png';
-     static const String hostelRoom = 'asset/hostelRoom.png';
-     static const String facilities = 'asset/facilitiesContainer.png';
-     static const String facilities1 = 'asset/facilities1.png';
      static const String arrow = 'asset/arrow.png';
+     static const String gallery = 'asset/gallery';
+     static const String plus = 'asset/Plus.png';
+     static const String minus = 'asset/Minus.png';
+     static const String rectangle = 'asset/Rectangle.png';
+     static const String uncheck = 'asset/uncheck.png';
      static const String dropdown = 'asset/dropdown.png';
+     static const String cross = 'asset/cross.png';
+     static const String blackarrow = 'asset/black.png';
+     static const String checkImage = 'asset/checkImage.png';
+     static const String hostelImage = 'asset/hostelRoom.png';
+     static const String share = 'asset/share.png';
+     static const String favourite = 'asset/favourite.png';
+     static const String location = 'asset/location.png';
+     static const String facilities = 'asset/facilities.png';
+     static const String facilities1 = 'asset/facilities1.png';
+     static const String human1 = 'asset/human1.png';
+     static const String humanBed = 'asset/humanBed.png';
+     static const String profile = 'asset/profile.png';
+     static const String backIcon1 = 'asset/backIcon1.png';
+     static const String profile1 = 'asset/profile1.png';
+
 }
