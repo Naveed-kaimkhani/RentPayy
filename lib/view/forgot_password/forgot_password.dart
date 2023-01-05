fb09193fbd6dd3a940a1aebab998d1001910a71f
Osama Malik <93467529+osamamalik234@users.noreply.github.com>
2023-01-04 20:18:09 -0800

    forget password updated

--- lib/view/forgot_password/forgot_password.dart
+++ lib/view/forgot_password/forgot_password.dart
@@ -1,15 +1,40 @@
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rentpayy/components/authButton.dart';
 import 'package:rentpayy/components/auth_screens_decor.dart';
+import 'package:rentpayy/components/circle_progress.dart';
 import 'package:rentpayy/components/custom_appbar.dart';
 import 'package:rentpayy/components/inputfields.dart';
+import 'package:rentpayy/utils/utils.dart';
 import '../../utils/style/AppColors.dart';
+import 'package:email_auth/email_auth.dart';
 
-class forgot_password extends StatelessWidget {
+class forgot_password extends StatefulWidget {
   forgot_password({Key? key}) : super(key: key);
 
+  @override
+  State<forgot_password> createState() => _forgot_passwordState();
+}
+
+class _forgot_passwordState extends State<forgot_password> {
   TextEditingController _emailController = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  bool isLoading = false;
+
+  Future<void> sentLink() async {
+    auth
+        .sendPasswordResetEmail(email: _emailController.text.toString())
+        .then((Value) {
+      utils.toastMessage('Sent a link');
+    }).onError((error, stackTrace) {
+      utils.flushBarErrorMessage("Something went wrong", context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,9 +53,7 @@ class forgot_password extends StatelessWidget {
                 padding: EdgeInsets.only(left: 41.w, right: 41.w),
                 child: Column(
                   children: [
-                    SizedBox(
-                      height: 32.h,
-                    ),
+                    SizedBox(height: 32.h),
                     Text(
                       "Forgot Password",
                       style: TextStyle(
@@ -56,10 +79,25 @@ class forgot_password extends StatelessWidget {
                     SizedBox(
                       height: 19.h,
                     ),
-                    authButton(
-                        text: "Send Code",
-                        func: () {},
-                        color: AppColors.primaryColor),
+                    isLoading
+                        ? circle_progress()
+                        : authButton(
+                            text: "Send Code",
+                            func: () {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              Future.delayed(Duration(seconds: 2), () {
+                                setState(() {
+                                  isLoading = false;
+                                });
+                              });
+                              _emailController.clear();
+                              sentLink();
+                              // _emailController.clear();
+                            },
+                            color: AppColors.primaryColor,
+                          ),
                   ],
                 ),
               ),
